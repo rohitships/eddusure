1c48322990a820737df026b91d1075cc88e2a577
Sachin Baguli <rohitbhaiya065@gmail.com>
2025-09-26 03:41:55 +0000

    the screen is not shpwing anything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,7 +145,7 @@ export default function TrustCheckPage() {
   }
 
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
        <Sidebar>
         <SidebarContent>
           <SidebarMenu>
@@ -161,10 +161,9 @@ export default function TrustCheckPage() {
             <Auth />
         </SidebarFooter>
       </Sidebar>
-      <div className="flex flex-col flex-1">
+      <SidebarInset>
         <Header />
-        <SidebarInset>
-          <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
             <AnalyticsDashboard />
             <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
               <div className="xl:col-span-1">
@@ -179,9 +178,8 @@ export default function TrustCheckPage() {
               </div>
             </div>
             <ActivityTracker />
-          </main>
-        </SidebarInset>
-      </div>
+        </main>
+      </SidebarInset>
     </div>
   );
 }
